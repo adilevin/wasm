ce0fc712d8a9f98d2b7a3a95507ef064a0b5a000
Adi Levin <alevin@aligntech.com>
2019-05-07 14:33:50 +0300

    Minor change in build_and_run.ps1

--- hello-world/build/build_and_run.ps1
+++ hello-world/build/build_and_run.ps1
@@ -1,6 +1,6 @@
 $project_root = (get-item .).parent.FullName
 $build_cmd = "docker run -v ${project_root}:/src -w /src/target -u emscripten trzeci/emscripten"
-$run_cmd   = "docker run --name emrun -p 6931:6931 -v ${project_root}:/src -w /src/target -u emscripten trzeci/emscripten"
+$exec_cmd   = "docker run --name emrun -p 6931:6931 -v ${project_root}:/src -w /src/target -u emscripten trzeci/emscripten"
 
 function Run {
     echo " "
@@ -16,9 +16,7 @@ Run $build_cmd cmake ../build
 Run $build_cmd make clean
 Run $build_cmd make
 
-#echo "docker rm -f emrun > null" > ../target/emrun.ps1
-#echo "$docker_cmd_for_execute emrun --hostname 0.0.0.0 --no_browser index.html" >> ../target/emrun.ps1
-docker rm -f emrun > null
-Run $run_cmd emrun --hostname 0.0.0.0 --no_browser index.html
+docker rm -f emrun > $null
+Run $exec_cmd emrun --hostname 0.0.0.0 --no_browser index.html
 
 pause
