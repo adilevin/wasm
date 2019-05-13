a852181a391ff64d424e2e07abaf69cd5960fcca
Adi Levin <alevin@aligntech.com>
2019-05-13 13:18:49 +0300

    Simplified build scripts

--- ccall-array/build/build_and_run.ps1
+++ ccall-array/build/build_and_run.ps1
@@ -1,21 +1,15 @@
 $project_root = (get-item .).parent.FullName
 $build_cmd = "docker run -v ${project_root}:/src -w /src/target -u emscripten trzeci/emscripten"
 $exec_cmd   = "docker run --name emrun -p 6931:6931 -v ${project_root}:/src -w /src/target -u emscripten trzeci/emscripten"
-
 function Run {
-    echo " "
-    echo ==================================================
-    echo "$args"
-    echo --------------------
-    
-    Invoke-Expression "$docker_build $args"
+    Write-Output " "
+    Write-Output "$args"
+    Invoke-Expression "$args" | ForEach-Object {'  ' + $_ }
 }
 
 mkdir ../target 2> $null 1>$null
 Run $build_cmd cmake ../build
-#Run $build_cmd make clean
 Run $build_cmd make
-copy ../src/index.html ../target/index.html
 
 docker rm -f emrun > $null
 Run $exec_cmd emrun --hostname 0.0.0.0 --no_browser index.html
